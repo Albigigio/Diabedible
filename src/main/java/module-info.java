444988c1bb418e70e4fee9fd2080efe6356e10bb
Albigigio <albertogagliardo95@gmail.com>
2025-05-22 11:34:41 +0200

    Start project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.diabedible {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.diabedible to javafx.fxml;
+    exports com.example.diabedible;
+}
